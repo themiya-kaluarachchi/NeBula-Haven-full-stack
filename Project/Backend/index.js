134e09e424a6e0684eb3fcbddf8bea50dacb6561
Themiya <themiyakaluarachchi@gmail.com>
2026-01-26 18:17:14 +0530

    add get post end point

--- Project/Backend/index.js
+++ Project/Backend/index.js
@@ -1,1 +1,21 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/', 
+    () => {
+        console.log("Get request received");
+    }
+);
+
+app.post('/', 
+    () => {
+        console.log("Post request received");
+    }   
+);
+
+app.listen(5000, 
+    () => {
+        console.log("Server is running");
+    }
+);

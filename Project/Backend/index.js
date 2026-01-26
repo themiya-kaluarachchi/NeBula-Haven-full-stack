b2343fd1f6bc30647ba858add7af5dd982736203
Themiya <themiyakaluarachchi@gmail.com>
2026-01-26 23:43:00 +0530

    add json test

--- Project/Backend/index.js
+++ Project/Backend/index.js
@@ -1,16 +1,32 @@
 import express from 'express';
 
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
   
 app.get('/', 
-    () => {
+    (req, res) => {
+        console.log(req.body);
         console.log("Get request received");
+
+        res.json(
+            {
+                message: "Get request successful"
+            }
+        );
     }
 );
 
-app.post('/', 
+app.post('/',  
     () => {
         console.log("Post request received");
+    }   
+);
+
+app.delete('/', 
+    () => {
+        console.log("Delete request received");
     }   
 );
 

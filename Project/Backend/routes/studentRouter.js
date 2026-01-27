d3f824752ed91d8dfd9d85850c898097802bd39c
Themiya <themiyakaluarachchi@gmail.com>
2026-01-27 19:35:13 +0530

    add

--- Project/Backend/routes/studentRouter.js
+++ Project/Backend/routes/studentRouter.js
@@ -13,3 +13,15 @@ studentRouter.post('/',
         console.log("POST request into student router");
     }
 );
+
+studentRouter.delete('/', 
+    () => {
+        console.log("DELETE request into student router");
+    }
+);
+
+studentRouter.put('/', 
+    () => {
+        console.log("PUT request into student router");
+    }
+);

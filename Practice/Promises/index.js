9d3abcebcc14657b70fedc99c43e22b5c9af52b3
Themiya <themiyakaluarachchi@gmail.com>
2026-01-30 05:02:06 +0530

    await

--- Practice/Promises/index.js
+++ Practice/Promises/index.js
@@ -1,1 +1,84 @@
-console.log("Promises in JavaScript");
+// console.log("Promises in JavaScript");
+// console.log("----------------------");
+
+// let winner = "Themiya Kaluarachchi";
+// let isJudgeBoardOkay = true;
+
+// const myPromise = new Promise(
+//     (resolve, reject) => {
+
+//         setTimeout(
+//             () => {
+//                 if (isJudgeBoardOkay) {
+//                     console.log("The winner is " + winner);
+//                     resolve({
+//                         name: winner,
+//                         prize: "Gold Medal",
+//                         marks: 98
+//                     })
+//                 } else {
+//                     reject({
+//                         reason: "You are Black",
+//                         message: "The judge board does not allow this participant."
+//                     })
+//                 }
+//             }, 5000
+//         )
+//     }
+// )
+
+// myPromise.then(
+//     (a) => {
+//         console.log(a);
+//         console.log(`Congratulations ${winner}, you have won the competition!`);
+//     }
+// ).catch (
+//     (error) => {
+//         console.log(error);
+//         console.log("Sorry, the judge board is not okay. Please try again later.");
+//     }
+// )
+
+
+
+function getUsers(dbPassword) {
+
+    const myPromise = new Promise(
+        (resolve, reject) => {
+
+            if (dbPassword == "123") {
+                setTimeout(
+                    () => {
+                        resolve(
+                            [
+                                { name: "Alice", age: 30 },
+                                { name: "Bob", age: 25 },
+                                { name: "Charlie", age: 35 }
+                            ]
+                        )
+                    }, 5000
+                );
+            } else {
+                reject(
+                    {
+                        message : "Invalid database password."
+                    }
+                )
+            }
+        }
+    );
+    return myPromise;
+}
+
+// getUsers("123").then(
+//     (users) => {
+//         console.log(users);
+//     }
+// ).catch (
+//     (error) => {
+//         console.log(error);
+//     }
+// )
+
+let response = await getUsers("123");
+console.log(response);

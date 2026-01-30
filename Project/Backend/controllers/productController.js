052f646f8152109fcd2fb14f6564507833b97885
Themiya <themiyakaluarachchi@gmail.com>
2026-01-31 02:30:35 +0530

    add create & get Product func to productController

--- Project/Backend/controllers/productController.js
+++ Project/Backend/controllers/productController.js
@@ -1,22 +1,30 @@
 import Product from "../models/product";
+import { isAdmin } from "./userController";
 
 export async function createProduct(req, res) {
 
-    if(req.user == null) {
-        res.status(401).json(
-            {
-                message : "Please login and try again"
-            }
-        )
-        return;
-    }
+    // if(req.user == null) {
+    //     res.status(401).json(
+    //         {
+    //             message : "Please login and try again"
+    //         }
+    //     )
+    //     return;
+    // }
 
-    if(req.user.role != "admin") {
-        res.status(403).json(
-            {
-                message : "You are not authorized to perform this action"
-            }
-        )
+    // if(req.user.role != "admin") {
+    //     res.status(403).json(
+    //         {
+    //             message : "You are not authorized to perform this action"
+    //         }
+    //     )
+    //     return;
+    // }
+
+    if(!isAdmin(req)) {
+        res.status(403).json({
+            message: "You are not authorized to perform this action"
+        });
         return;
     }
 
@@ -41,3 +49,16 @@ export async function createProduct(req, res) {
         );
     }
 }  
+
+export async function getProducts(req, res) {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json(
+            {
+                message : "Failed to retrieve products: " + error.message
+            }
+        );
+    }
+}

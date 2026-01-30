f28122e121053e96a69593a78deee41e241bc5b5
Themiya <themiyakaluarachchi@gmail.com>
2026-01-31 02:11:20 +0530

    add product model

---
+++ Project/Backend/models/product.js
@@ -0,0 +1,41 @@
+
+
+const productShema = new mongoose.Schema(
+    {
+        productID : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        name : {
+            type : String,
+            required : true
+        },
+        altNames : {
+            type : [String],
+            default : [],
+            required : true
+        },
+        description : {
+            type : String,
+            required : true
+        },
+        images : {
+            type : [String],
+            default : [],
+            required : true
+        },
+        price : {
+            type : Number,
+            required : true
+        },
+        labelledPrice : {
+            type : Number,
+            required : true
+        }
+    }
+)
+
+const Product = mongoose.model("Product", productShema);
+
+export default Product;

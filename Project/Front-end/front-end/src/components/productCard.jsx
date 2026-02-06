9f233fa3280baea117c334ded3dfece2bf1a7d36
Themiya <themiyakaluarachchi@gmail.com>
2026-02-07 01:20:32 +0530

    add productCard components

---
+++ Project/Front-end/front-end/src/components/productCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard() {
+
+    return (
+        <div>
+            <h1>Apple iPad</h1>
+            <p>Price : $599</p>
+            <img src="https://celltronics.lk/wp-content/uploads/2025/04/Apple-iPad-Air-11-inch-M3-Chip-600x600.jpg" alt="" />
+            <button>Add to Cart</button>
+        </div>
+    )
+}

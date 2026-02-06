37df4e49d1c40709fd00acc2a34fbba017519b0f
Themiya <themiyakaluarachchi@gmail.com>
2026-02-07 01:40:50 +0530

    add props

--- Project/Front-end/front-end/src/App.jsx
+++ Project/Front-end/front-end/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import ProductCard from './components/productCard'
 
 function App() {
 
   return (
     <>
       <h1>KTK</h1>
+      <ProductCard name="Apple iPad" price="$599" image="https://celltronics.lk/wp-content/uploads/2025/04/Apple-iPad-Air-11-inch-M3-Chip-600x600.jpg" />
+      <ProductCard name="Samsung Galaxy Tab" price="$499" image="https://objectstorage.ap-mumbai-1.oraclecloud.com/n/softlogicbicloud/b/cdn/o/products/MBPSSM-X216BZAASLK-S--1--1702023887.jpg" />
     </>
   )
 }
--- Project/Front-end/front-end/src/components/productCard.jsx
+++ Project/Front-end/front-end/src/components/productCard.jsx
@@ -1,10 +1,12 @@
-export default function ProductCard() {
+import "./productCard.css"
+
+export default function ProductCard(props) {
 
     return (
-        <div>
-            <h1>Apple iPad</h1>
-            <p>Price : $599</p>
-            <img src="https://celltronics.lk/wp-content/uploads/2025/04/Apple-iPad-Air-11-inch-M3-Chip-600x600.jpg" alt="" />
+        <div className="productCard">
+            <h1>{props.name}</h1>
+            <p>Price : {props.price}</p>
+            <img className="productImage" src={props.image} alt="" />
             <button>Add to Cart</button>
         </div>
     )

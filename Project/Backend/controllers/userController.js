c316b36b21d3306bdb180a27df2f6bbd11cba0b5
Themiya <themiyakaluarachchi@gmail.com>
2026-03-04 11:18:15 +0530

    started again

--- Project/Backend/controllers/userController.js
+++ Project/Backend/controllers/userController.js
@@ -83,3 +83,13 @@ export function isAdmin(req) {
     }
     return true;
 }
+
+export function isCustomer(req) {
+    if(req.user == null) {
+        return false;
+    } 
+    if(req.user.role != "user") {
+        return false;
+    }
+    return true;
+}
--- Project/Front-end/front-end/src/App.jsx
+++ Project/Front-end/front-end/src/App.jsx
@@ -4,6 +4,7 @@ import ProductCard from "./components/productCard";
 import AdminPage from "./pages/adminPage";
 import HomePage from "./pages/homePage";
 import TestPage from "./pages/test";
+import LoginPage from "./pages/loginPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes path="/">
           <Route path="/*" element={<HomePage />} />
           <Route path="/register" element={<h1>Register Page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/test" element={<TestPage />} />
         </Routes>

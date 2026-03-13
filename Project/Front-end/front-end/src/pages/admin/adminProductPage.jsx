c8f80263155fd1699da9226520f9bdd276507097
Themiya <themiyakaluarachchi@gmail.com>
2026-03-14 03:48:51 +0530

    fetch and display products in admin product page

--- Project/Front-end/front-end/src/pages/admin/adminProductPage.jsx
+++ Project/Front-end/front-end/src/pages/admin/adminProductPage.jsx
@@ -1,7 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+
+
 export default function AdminProductPage() {
+  const [products, setProducts] = useState([]);
+
+    axios.get(import.meta.env.VITE_API_URL + "/api/products").then(
+      (response) => {
+        setProducts(response.data);
+      }
+    );
+
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Admin Product Management</h1>
+    <div className="w-full h-full p-[10px]">
+      <table className="border w-full text-center">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Product Price</th>
+            <th>Labelled Price</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => {
+            return (
+              <tr key={item.productID}>
+                <td>
+                  <img src={item.images[0]} alt={item.name} className="w-16 h-16 object-cover rounded py-[5px]" />
+                </td>
+                <td>{item.productID}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.labelledPrice}</td>
+                <td>{item.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }

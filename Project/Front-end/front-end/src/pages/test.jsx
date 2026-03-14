035a59c2ec6a7569d661dc5c6fad37e10d7362e3
Themiya <themiyakaluarachchi@gmail.com>
2026-03-14 12:32:22 +0530

    refactor image upload functionality and create mediaUpload utility

--- Project/Front-end/front-end/src/pages/test.jsx
+++ Project/Front-end/front-end/src/pages/test.jsx
@@ -1,25 +1,13 @@
-import { createClient } from "@supabase/supabase-js";
 import React, { useState } from "react";
-
-const annonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqdnpzZmlkY2dnd3lkZ3Z2dXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NTM1MDAsImV4cCI6MjA4OTAyOTUwMH0.yrHfTm5sD3kku1YtGiGatuMOF3PUwEh7HLqGZYatlsI"
-const supabaseUrl = "https://sjvzsfidcggwydgvvuuf.supabase.co"
-
-const superbase = createClient(supabaseUrl, annonKey)
+import mediaUpload from "../utils/mediaUpload";
 
 export default function TestPage() {
 
   const [file, setFile] = useState(null);
 
-  function uploadImage() {
-    superbase.storage.from("images").upload(file.name, file , {
-      upsert: false,
-      cacheControl: "3600",
-    }).then(
-      () => {
-        const publicUrl = superbase.storage.from("images").getPublicUrl(file.name).data.publicUrl
-        console.log(publicUrl);
-      }
-    )
+  async function uploadImage() {
+    const link = await mediaUpload(file);
+    console.log(link);
   }
 
   return (
---
+++ Project/Front-end/front-end/src/utils/mediaUpload.jsx
@@ -0,0 +1,39 @@
+import { createClient } from "@supabase/supabase-js"
+
+const annonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqdnpzZmlkY2dnd3lkZ3Z2dXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NTM1MDAsImV4cCI6MjA4OTAyOTUwMH0.yrHfTm5sD3kku1YtGiGatuMOF3PUwEh7HLqGZYatlsI"
+const supabaseUrl = "https://sjvzsfidcggwydgvvuuf.supabase.co"
+
+const superbase = createClient(supabaseUrl, annonKey)
+
+// superbase.storage.from("images").upload(file.name, file , {
+//       upsert: false,
+//       cacheControl: "3600",
+//     }).then(
+//       () => {
+//         const publicUrl = superbase.storage.from("images").getPublicUrl(file.name).data.publicUrl
+//         console.log(publicUrl);
+//       }
+//     )
+
+export default function mediaUpload(file) {
+    return new Promise(
+        (resolve, reject) => {
+            if (file == null) {
+                reject("No file selected");
+            } else {
+                superbase.storage.from("images").upload(file.name, file , {
+                    upsert: false,
+                    cacheControl: "3600",
+                }).then (() => {
+                    const publicUrl = superbase.storage.from("images").getPublicUrl(file.name).data.publicUrl;
+                    resolve(publicUrl);
+                }).catch (
+                    () => {
+                        reject("Upload failed");
+                    }
+                )
+            }
+        }
+    )
+}
+
